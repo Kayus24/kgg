21e7ef8a317273f19b7d9fa6f5d9d1b9f71a12b9
Kayus24 <kullmannmaximikian@gmail.com>
2026-07-27 11:43:25 +0200

    PWA: align update lifecycle with v64

--- patient-version-label.js
+++ patient-version-label.js
@@ -1,6 +1,6 @@
 (()=>{
   const ID='kggAppVersion';
-  const RELEASE='62';
+  const RELEASE='64';
   const BANNER_ID='kggUpdateGate';
   const RELOAD_KEY='kgg-sw-reload-v'+RELEASE;
   const UPDATE_TIMEOUT_MS=8000;
